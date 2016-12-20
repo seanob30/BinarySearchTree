a0f6e7b9974e23cd83d3dab7a3edc3617e323dcd
seanob30 <seanobrien2014@gmail.com>
2016-12-20 16:21:28 -0600

    add via loop or recursion, organize based off value

---
+++ BinarySearchTree/Tree.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    class Tree
+    {
+        Node top;
+
+        public Tree()
+        {
+            top = null;
+        }
+        public Tree(int initial)
+        {
+            top = new Node(initial);
+        }
+        public void Add(int value)
+        {
+            if (top == null)
+            {
+                Node newNode = new Node(value);
+                top = newNode;
+                return;
+            }
+            Node currentNode = top;
+            bool check = false;
+            do
+            {
+               if (value < currentNode.value)
+                {
+                    if (currentNode.left == null)
+                    {
+                        Node newNode = new Node(value);
+                        currentNode.left = newNode;
+                        check = true;
+                    }
+                    else
+                    {
+                        currentNode = currentNode.left;
+                    }
+                }
+               if (value >= currentNode.value)
+                {
+                    if (currentNode.right == null)
+                    {
+                        Node newNode = new Node(value);
+                        currentNode.right = newNode;
+                        check = true;
+                    }
+                    else
+                    {
+                        currentNode = currentNode.right;
+                    }
+                }
+            } while (!check);
+        }
+        public void AddRec(int value)
+        {
+            AddRecLogic(ref top, value);
+        }
+        public void AddRecLogic(ref Node node, int value)
+        {
+            if (node == null)
+            {
+                Node newNode = new Node(value);
+                node = newNode;
+                return;
+            }
+            if (value < node.value)
+            {
+                AddRecLogic(ref node.left, value);
+                return;
+            }
+            if (value >= node.value)
+            {
+                AddRecLogic(ref node.right, value);
+                return;
+            }
+        }
+
+    }
+}
